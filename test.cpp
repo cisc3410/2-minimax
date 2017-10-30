569037cb494b9874c5a0e58827e81166240b43e7
rivlev <rivka.levitan@gmail.com>
2017-10-30 14:32:27 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,6 +6,9 @@ int main(int argc, char** argv) {
   std::vector<char> v = {'x','x','x','-','o','-','-','-','-'};
   TicTacToeBoard board(v.begin(), v.end());
   
+  TicTacToeAgent a('x'), b('o');
+  
   char c;
   std::cout << board.won(&c) << ' ' << c << std::endl;
+  std::cout << a.Utility(board) << ' ' << b.Utility(board) << std::endl;
 }
